d95ed92f2471b4c62446944e88e65c4c129ec2cc
mike1808 <arm.localhost@gmail.com>
2013-06-03 21:47:32 +0400

    added available funds input a little bit changed logic (now availableSeats items is classes) decorated with bootstrap

--- TicketBooking/Scripts/ticketBookingViewModel.js
+++ TicketBooking/Scripts/ticketBookingViewModel.js
@@ -6,38 +6,48 @@
         // Editable data
         self.availableSeats = ko.observableArray(makeSeats());
         self.bookedSeats = ko.observableArray([
-            new seatBooking('', self.availableSeats)
+            new SeatBooking('', self.availableSeats)
         ]);
-        
+        self.totalFunds = ko.observable(1000);
+
         // Computed data
         self.totalPrice = ko.computed(function() {
             var total = 0;
             for (var i = 0; i < self.bookedSeats().length; i++) {
                 total += self.bookedSeats()[i].seat().price;
+                
             }
-            return total ? '$' + total : 0;
+            return total ? '$' + total.toFixed(0) : 0;
+        });
+        self.availableFunds = ko.computed(function () {
+            var left =  self.totalFunds() - self.totalPrice().toString().substr(1);
+
+            return left;
         });
 
         // Operations
         self.addSeat = function () {
-            self.availableSeats.remove(self.seat); // Removing this seat from available seats
-            self.bookedSeats.push(new seatBooking('', self.availableSeats));
+            //self.availableSeats.remove(seat); // Removing this seat from available seats
+ 
+            self.bookedSeats.push(new SeatBooking('', self.availableSeats));
         };
-        self.removeSeat = function(seat) {
-            self.availableSeats.push(seat); // Adding this seat to availabes seats
-            self.bookedSeats.remove(seat);
+        self.removeSeat = function (bookedSeat) {
+            //self.availableSeats.push(bookedSeat.seat); // Adding this seat to availabes seats
+            
+
+            self.bookedSeats.remove(bookedSeat);
         };
     }
 
-    // Function for displaying data in the booking table
-    function seatBooking(name, seat) {
+    // Class for displaying data in the booking table
+    function SeatBooking(name, seat) {
         var self = this;
         self.name = name;
         self.seat = ko.observable(seat);
         
         self.formattedPrice = ko.computed(function () {
             var price = self.seat().price || 0;
-            return '$' + price;
+            return '$' + price.toFixed(0);
         });
     }
     
@@ -47,16 +57,46 @@
         for (var i = 0; i < 3; i++) {
             for (var j = 0; j < 3; j++) {
                 var seatIndex = String.fromCharCode('A'.charCodeAt() + j) + (i + 1);
-                var seat = {
-                    index: seatIndex,
-                    price: (i + 1) * 100 + (50 - 10 * (j % 2)) // Some expression for genrating price
-                };
+                var price = Math.random() * 400 + 100;
+                var seat = new Seat(seatIndex, price);
                 seats.push(seat);
             }
         }
         return seats;
     }
+    // Class for seat
+    function Seat(index, price) {
+        var self = this;
+        self.index = index;
+        self.price = price;
+    }
 
+    // Custom binding for smooth showing waring messasge
+    ko.bindingHandlers.fadeWaring = {
+        update: function(element, valueAccessor) {
+          var shouldDisplay = valueAccessor();
+          shouldDisplay ? $(element).fadeIn() : $(element).fadeOut(0);
+      }  
+    };
+
+    // Custom binding for glowing funds if they are over
+    ko.bindingHandlers.glow = {
+        update: function(element, valueAccessor) {
+            var shouldGlow = valueAccessor();
+            if (shouldGlow) {
+                $(element).css({
+                    color: 'red',
+                    'font-weight': 'bold'
+                });
+
+            } else {
+                $(element).css({
+                    color: 'black',
+                    'font-weight': 'normal'
+                });
+            }
+        }  
+    };
 
     ko.applyBindings(new bookingViewModel());
 });
